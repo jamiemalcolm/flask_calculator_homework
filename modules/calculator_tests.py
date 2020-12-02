5961be81a35ffdb148c842bd178f1198ff10d24e
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-02 17:43:03 +0000

    second add test passed

--- modules/calculator_tests.py
+++ modules/calculator_tests.py
@@ -7,3 +7,7 @@ class TestCalculatorFunctions(unittest.TestCase):
     def test_add_function_returns_10(self):
         returns_10 = add(5, 5)
         self.assertEqual(10, returns_10)
+    
+    def test_add_returns_6000(self):
+        returns_6000 = add(4031, 1969)
+        self.assertEqual(6000, returns_6000)
