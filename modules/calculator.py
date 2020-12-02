3232aae4af954f7e372fd2a4a718c3029a922585
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-02 17:48:03 +0000

    divide function passed

--- modules/calculator.py
+++ modules/calculator.py
@@ -3,3 +3,6 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     return num1 - num2
+
+def divide(num1, num2):
+    return num1 // num2
--- modules/calculator_tests.py
+++ modules/calculator_tests.py
@@ -15,3 +15,7 @@ class TestCalculatorFunctions(unittest.TestCase):
     def test_subtract_returns_6(self):
         returns_6 = subtract(10, 4)
         self.assertEqual(6, returns_6)
+
+    def test_divide_returns_2(self):
+        returns_2 = divide(10, 5)
+        self.assertEqual(2, returns_2)
