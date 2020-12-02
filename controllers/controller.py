c5eec93b78d709d974e30fbf8ec7e7cea3703d65
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-02 18:18:07 +0000

    first route working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,7 @@
 from app import app
+from modules.calculator_tests import *
 
-
+@app.route('/add/<number1>/<number2>')
+def addition(number1, number2):
+    return f"this equals {add(number1, number2)}"
+    
--- modules/calculator.py
+++ modules/calculator.py
@@ -1,14 +1,23 @@
 def add(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
     return num1 + num2
 
 def subtract(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
     return num1 - num2
 
 def divide(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
     return num1 // num2
 
 def multiply(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
     return num1 * num2
 
 def squared(num):
+    num = int(num)
     return num**2
